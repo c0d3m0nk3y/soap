6ab5f30dc68771b7d2f24227923349086bfdca8f
c0d3m0nk3y <rickyoswald@gmail.com>
2016-11-24 22:15:02 +0000

    first commit

---
+++ client.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+$client = new SoapClient('stockquote.wsdl');
+
+print($client->getQuote('ibm'));
+
+?>
---
+++ server.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+ini_set('soap.wsdl_cache_enabled', '0');
+
+$quotes = array('ibm'=>180.0);
+
+function getQuote($symbol) {
+	global $quotes;
+	return $quotes[$symbol];
+}
+
+$server = new SoapServer('sotckquote.wsdl');
+$server->addFunction('getQuote');
+$server->handle();
+
+?>
